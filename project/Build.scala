0609051445fbd51f58bb8a5307eb53938e87eaa2
Wei Zhang <xbbl21i@ml07dtsg1403817.ams.bnymellon.net>
2017-05-23 09:58:14 -0400

    upgrade to akka 2.5.1

--- project/Build.scala
+++ project/Build.scala
@@ -61,9 +61,9 @@ object PublishingSettings {
 }
 
 object Version {
-  
+
   val scala      = "2.11.8"
-  val akka       = "2.5.0"
+  val akka       = "2.5.1"
   val spray      = "1.3.3"
   val commnsLang = "3.4"
   val scalaTest  = "3.0.1"
